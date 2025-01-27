1bc912288a1589fefea0b2e0cbb226b7d68633cb
rayan2125 <dhananjay.s.dubey20125@gmail.com>
2025-01-27 23:32:03 +0530

    first commit

---
+++ src/app/about.tsx
@@ -0,0 +1,8 @@
+
+
+const About :React.FC=()=>{
+    return(
+        <h1>testing</h1>
+    )
+}
+export default About;
---
+++ src/app/dashboard/pag.tsx
@@ -0,0 +1,8 @@
+
+
+const Dashboard :React.FC=()=>{
+return(
+    <h1>hello world</h1>
+)
+}
+export default Dashboard
