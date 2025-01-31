e44b1f22766d6ad1a02bc74349961cadf43faa26
rayan2125 <dhananjay.s.dubey20125@gmail.com>
2025-01-31 23:52:44 +0530

    ant deisg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,6 @@
 import { redirect } from 'next/navigation';
 
 export default function Page(): never {
+
   redirect('/dashboard');
 }
--- src/components/sidebar/page.tsx
+++ src/components/sidebar/page.tsx
@@ -1,20 +1,45 @@
-"use client"
+"use client";
 
-import { useRouter } from "next/navigation"
+import { Avatar, Flex, Splitter, Typography } from "antd";
+import Image from "next/image";
+import React from "react";
+import { UserOutlined } from '@ant-design/icons';
+const Desc: React.FC<{ text?: string | number }> = ({ text }) => (
+  <Flex justify="center" align="center" style={{ height: "100%" }}>
+    <Typography.Text type="secondary" style={{ whiteSpace: "nowrap" }}>
+      {text}
+    </Typography.Text>
+  </Flex>
+);
+const Profile :React.FC =({})=>(
+    <Flex justify="center" align="center" style={{height:'100%'}}>
+<Avatar icon={<UserOutlined/>} />
+<Typography.Text >
+    Ramesh 
+</Typography.Text>
+    </Flex>
+)
+const Sidebar: React.FC = () => {
+  return (
+    <section className="p-10">
+      <Splitter style={{ height: 200, boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)" }}>
+        <Splitter.Panel defaultSize="20%" min="20%" max="70%" className="bg-red-100">
+          {/* <Desc text="First" /> */}
+          <Profile/>
+        </Splitter.Panel>
+        <Splitter.Panel>
+          <Splitter layout="vertical">
+            <Splitter.Panel className="bg-blue-100">
+              <Desc text="Second" />
+            </Splitter.Panel>
+            <Splitter.Panel className="bg-blue-100">
+              <Desc text="Third" />
+            </Splitter.Panel>
+          </Splitter>
+        </Splitter.Panel>
+      </Splitter>
+    </section>
+  );
+};
 
-
-const Sidebar :React.FC=()=>{
-    const navigation = useRouter()
-    const handleconsole=()=>{
-        navigation.push("/employee")
-    }
-    return(
-<section className="">
-    <div>
-        <h1>hhh</h1>
-        <button onClick={handleconsole}>test</button>
-    </div>
-</section>
-    )
-}
-export default Sidebar
+export default Sidebar;
